b33b96fd5586409c947ad2131513a3127984acbb
Naveen <nvnjwl@gmail.com>
2015-09-26 16:25:48 +0530

    Changes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,73 @@
-(function(){
+(function () {
 	'use strict';
-	var app=angular.module("mymap",[]);
-	app.controller("mapcontroller",function($scope){
+	var app = angular.module("mymap", []);
+	app.controller("mapcontroller", function ($scope,$http) {
+
+		$scope.data = "welcome";
+		$scope.cities=[];
+		$scope.category=[];
+		$scope.subcategory=[];
 		
-		$scope.data="welcome";
-		$scope.submitCurrent=function(){
+		$scope.submitCurrent = function () {
 			//alert("Function");
-			var city=document.getElementById("cityname").value;
-			var category=document.getElementById("category").value;
-			var subcategory=document.getElementById("subcategory").value;
-			alert("Function"+city+category+subcategory);
+			var city = document.getElementById("cityname").value;
+			var category = document.getElementById("category").value;
+			var subcategory = document.getElementById("subcategory").value;
+			alert("Function" + city + category + subcategory);
 		}
+
+		$http({
+			url: 'http://jsonstub.com/data',
+			method: 'GET',
+			dataType: 'json',
+			data: '',
+			headers: {
+				'Content-Type': 'application/json',
+				'JsonStub-User-Key': '34e5429c-43a1-4485-9423-70ac4e393ba9',
+				'JsonStub-Project-Key': 'f3b71264-4952-4db0-a7a4-ea183e4cedcd'
+			}
+		}).success(function (data, status, headers, config) {
+			//console.log(JSON.stringify(data, null, 4));
+		}).error(function(){
+			console.log("Some Error Came");
+		});
+
+		$http({
+			url: 'cityname.json',
+			method: 'GET',
+			dataType: 'json',
+			data: ''
+		}).success(function (data, status, headers, config) {
+			//console.log(JSON.stringify(data, null, 4));
+			$scope.cities=data.cityname;
+		}).error(function(){
+			console.log("Some Error Came");
+		});
+
+		$http({
+			url: 'category.json',
+			method: 'GET',
+			dataType: 'json',
+			data: ''
+		}).success(function (data, status, headers, config) {
+			//console.log(JSON.stringify(data, null, 4));
+			$scope.category=data.category;
+		}).error(function(){
+			console.log("Some Error Came");
+		});
 		
+		$http({
+			url: 'subcategory.json',
+			method: 'GET',
+			dataType: 'json',
+			data: ''
+		}).success(function (data, status, headers, config) {
+			//console.log(JSON.stringify(data, null, 4));
+			$scope.subcategory=data.subcategory;
+		}).error(function(){
+			console.log("Some Error Came");
+		});
+		
+
 	});
 })();
--- js/mymap.js
+++ js/mymap.js
@@ -1,71 +1,72 @@
-function initialize() {
 
-	var myLatLng = {
-		lat: 28.480342,
-		lng: 77.107700
-	};
+	function initialize() {
 
-	var mapProp = {
-		center: new google.maps.LatLng(28.480342, 77.107700),
-		zoom: 7,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+		var myLatLng = {
+			lat: 28.480342,
+			lng: 77.107700
+		};
 
-	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+		var mapProp = {
+			center: new google.maps.LatLng(28.480342, 77.107700),
+			zoom: 9,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
 
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: 'OLX Listing'
-	});
+		var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-	var citymap = {
-		delhi: {
-			center: {
-				lat: 28.480342,
-				lng: 77.107700
-			},
-			population: 100
-		},
-		gurgoan: {
-			center: {
-				lat: 28.480342,
-				lng: 77.107710
-			},
-			population: 200
-		}
-	};
-
-
-
-	// Construct the circle for each value in citymap.
-	// Note: We scale the area of the circle based on the population.
-	for (var city in citymap) {
-		// Add the circle for this city to the map.
-		var cityCircle = new google.maps.Circle({
-			strokeColor: '#FF0000',
-			strokeOpacity: 0.8,
-			strokeWeight: 2,
-			fillColor: '#FF0000',
-			fillOpacity: 0.35,
+		var marker = new google.maps.Marker({
+			position: myLatLng,
 			map: map,
-			center: citymap[city].center,
-			radius: 400
+			title: 'OLX Listing'
 		});
+
+		var citymap = {
+			delhi: {
+				center: {
+					lat: 28.480342,
+					lng: 77.107700
+				},
+				listingCount: 100
+			},
+			gurgoan: {
+				center: {
+					lat: 28.480342,
+					lng: 77.107710
+				},
+				listingCount: 200
+			}
+		};
+
+
+
+		// Construct the circle for each value in citymap.
+		// Note: We scale the area of the circle based on the population.
+		for (var city in citymap) {
+			// Add the circle for this city to the map.
+			var cityCircle = new google.maps.Circle({
+				strokeColor: '#FF0000',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#FF0000',
+				fillOpacity: 0.35,
+				map: map,
+				center: citymap[city].center,
+				radius: 4000
+			});
+		}
+
 	}
 
-}
 
 
 
 
 
+	function loadScript() {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+		document.body.appendChild(script);
+	}
 
-function loadScript() {
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-	document.body.appendChild(script);
-}
-
-window.onload = loadScript;
+	window.onload = loadScript;
